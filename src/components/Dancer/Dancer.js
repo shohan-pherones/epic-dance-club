7884956f7e85f858371234c56cf9cc147e8b177e
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-10 23:49:09 +0600

    dancers added

---
+++ src/components/Dancer/Dancer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Dancer.css";
+
+const Dancer = (props) => {
+  const { name, nationality, bodyMeasurements, age, fees, image } =
+    props.dancer;
+
+  return (
+    <div className="dancer">
+      <div className="dancer-image">
+        <img src={image} alt={name} />
+      </div>
+      <div className="dancer-details">
+        <h2>{name}</h2>
+        <p>{nationality} Dancer</p>
+        <p>Age: {age}</p>
+        <p>Body: {bodyMeasurements}</p>
+        <p>
+          Fees: <span className="fees">${fees}</span>
+        </p>
+        <button>Pick Her</button>
+      </div>
+    </div>
+  );
+};
+
+export default Dancer;
--- src/components/Dancers/Dancers.js
+++ src/components/Dancers/Dancers.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Dancer from "../Dancer/Dancer";
 import "./Dancers.css";
 
 const Dancers = () => {
+  const [dancers, setDancers] = useState([]);
+
+  useEffect(() => {
+    fetch("./dancers.json")
+      .then((res) => res.json())
+      .then((data) => setDancers(data));
+  }, []);
   return (
     <div className="dancers-container">
-      <div className="dancers"></div>
+      <div className="dancers">
+        {dancers.map((dancer) => (
+          <Dancer key={dancer.id} dancer={dancer}></Dancer>
+        ))}
+      </div>
       <div className="summary">
         <h2>Picking Summary</h2>
         <p>Dancer(s) Picked: </p>
